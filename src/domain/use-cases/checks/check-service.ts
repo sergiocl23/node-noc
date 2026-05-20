4b51854cc8d8d101f2e817fb11003ea1de9daae4
Sergio Cerda <sergio.cerda.lozano@gmail.com>
2026-05-20 01:47:05 -0400

    fin seccion 10

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,14 +1,17 @@
+import { LogEntity, LogSeverityLevel } from '../../entities/log.entity';
+import { LogRepository } from '../../repository/log.repository';
 interface CheckServiceUseCase{
     execute( url: string ): Promise<boolean>;
 }
 
-type SuccessCallback = () => void;
-type ErrorCallback = ( error: string ) => void;
+type SuccessCallback = (() => void) | undefined;
+type ErrorCallback = (( error: string ) => void) | undefined;
 
 
 export class CheckService implements CheckServiceUseCase{
 
     constructor(
+        private readonly logRepository: LogRepository,
         private readonly successCallback: SuccessCallback,
         private readonly errorCallback: ErrorCallback
     ) {}
@@ -20,12 +23,26 @@ export class CheckService implements CheckServiceUseCase{
             if ( !req.ok ) {
                 throw new Error( `Error on check service ${ url }` );
             }
-            this.successCallback();
+            
+            const log = new LogEntity({
+                message: `Service ${ url } working`, 
+                level: LogSeverityLevel.low,  
+                origin: 'check-service.ts'
+            });
+            this.logRepository.saveLog( log );
+            this.successCallback && this.successCallback();
+
             return true;
         } catch (error) {
+            const errorMessage = `${url} is not ok. ${ error }`;
+            const log = new LogEntity({
+                message: errorMessage , 
+                level: LogSeverityLevel.high,
+                origin: 'check-service.ts'
+            });
+            this.logRepository.saveLog(log);
 
-            console.log(`${ error }`);
-            this.errorCallback( `${ error }` );
+            this.errorCallback && this.errorCallback( errorMessage );
             return false;
         }
 
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,13 @@
-import { CheckService } from "../domain/use-cases/checks/check-service";
-import { CronService } from "./cron/cron-service";
+import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
+import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
+import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
+import { EmailService } from "./email/email.service";
+
+const fileSystemLogRepository = new LogRepositoryImpl(
+  new FileSystemDatasource(),
+);
+
+const emailService = new EmailService();
 
 export class Server {
 
@@ -7,19 +15,33 @@ export class Server {
 
         console.log('Server started ...');
 
-        CronService.createJob(
-            '*/5 * * * * *',
-            () => {
-                const url = 'https://google.com';
-                new CheckService(
-                    () => console.log(`${ url } is ok`),
-                    ( error ) => console.log( error ),
-                ).execute(url);
-                // new CheckService().execute('http://localhost:3000/posts');
 
-            }
+        // Mandar email
+        new SendEmailLogs(
+            emailService,
+            fileSystemLogRepository
+        ).execute(
+            ['andres.lozano290820@gmail.com']
+        )
+        // emailService.sendEmailWithFileSystemLogs(
+        //     ['andres.lozano290820@gmail.com', 'sergio.random187@gmail.com']
+        // );
 
-        );
+        // CronService.createJob(
+        //     '*/5 * * * * *',
+        //     () => {
+        //         // const url = 'https://google.com';
+        //         const url = 'http://localhost:3000/posts';
+        //         new CheckService(
+        //             fileSystemLogRepository,
+        //             () => console.log(`${ url } is ok`),
+        //             ( error ) => console.log( error ),
+        //         ).execute(url);
+        //         // new CheckService().execute('http://localhost:3000/posts');
+
+        //     }
+
+        // );
 
     }
 
